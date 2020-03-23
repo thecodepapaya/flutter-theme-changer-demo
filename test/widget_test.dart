a9cceb5cc71557bdb96cf417159c2baff7658169
ashutoshsingh05 <201851029@iiitvadodara.ac.in>
2020-03-24 01:16:52 +0530

    Added tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,16 +15,44 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(ThemeChanger());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    final key = find.byKey(ValueKey('themeSwitch'));
+    expect(key, findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    final changedState =
+        tester.state<ThemeChangerState>(find.byType(ThemeChanger));
+    expect(changedState.currentTheme, ThemeData.light());
+    expect(changedState.lightTheme, true);
+
+    await tester.tap(key);
     await tester.pump();
+    expect(key, findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    final changedState2 =
+        tester.state<ThemeChangerState>(find.byType(ThemeChanger));
+    expect(changedState2.currentTheme, ThemeData.dark());
+    expect(changedState2.lightTheme, false);
+
+    await tester.tap(key);
+    await tester.pump();
+    expect(key, findsOneWidget);
+
+    final changedState3 =
+        tester.state<ThemeChangerState>(find.byType(ThemeChanger));
+    expect(changedState3.currentTheme, ThemeData.light());
+    expect(changedState3.lightTheme, true);
+
+    // expect(currentTheme, matcher)
+
+    // // Verify that our counter starts at 0.
+    // expect(find.text('0'), findsOneWidget);
+    // expect(find.text('1'), findsNothing);
+
+    // // Tap the '+' icon and trigger a frame.
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
+
+    // // Verify that our counter has incremented.
+    // expect(find.text('0'), findsNothing);
+    // expect(find.text('1'), findsOneWidget);
   });
 }
